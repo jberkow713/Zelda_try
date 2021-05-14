1fad2e85e4a75421332d00f6585be3975833eeb2
jesse berkowitz <jberkow@brandeis.edu>
2021-05-14 17:54:37 -0400

    got the movmenet parameters working finally

--- movement.py
+++ movement.py
@@ -6,21 +6,53 @@ from pygame.locals import(
     K_ESCAPE, KEYDOWN, QUIT
 )
 size = width, height = 800, 800
-centerx = size[0] *.5
-centery = size[1] *.5
-center = (centerx, centery)
+positionx = size[0] *.5
+positiony = size[1] *.5
+position = (positionx, positiony)
 speed = [1.5, 2.25]
 black = (0,0,0)
 blue = (0,0,255)
 turquoise= (0,255,255)
 
+
+RESCALE_WIDTH = 100
+RESCALE_HEIGHT = 100
+
 pygame.init()
 
 screen = pygame.display.set_mode(size)
 ball = pygame.image.load("BEACH_BALL.jpg")
-ball = pygame.transform.scale(ball, (50, 50))
+ball = pygame.transform.scale(ball, (RESCALE_WIDTH, RESCALE_HEIGHT))
 #Setting player's position in center of map
-player = ball.get_rect(topleft=center)
+player = ball.get_rect(topleft=position)
+
+
+def check_coordinates(positionx, positiony, board_width, board_height, obj_width, obj_height):
+    
+    position = (positionx, positiony)
+        
+    max_width = board_width-obj_width
+    max_height = board_height-obj_height
+
+    if position[0]<0:
+        positionx = 0
+        return positionx, positiony 
+    if position[0]>max_width:
+        positionx = max_width
+        return positionx, positiony
+    if position[1]<0:
+        positiony = 0
+        return positionx, positiony
+    if position[1]>max_height:
+        positiony = max_height
+        return positionx, positiony
+
+    return positionx, positiony                   
+
+
+
+
+
 
 
 running = True
@@ -34,38 +66,45 @@ while running:
               
             
             if event.key == pygame.K_DOWN:
-                centerx += 50
-                centery +=50
-                player.center = (centerx, centery)
-                if player.left <0:
-                    player.left = 0
-                    centerx = player.left
-                if player.right >width:
-                    player.right = width
-                    centerx = player.right    
-                if player.top <0:
-                    player.top = 0
-                    centery = player.top
-                if player.bottom > height:
-                    player.bottom = height
-                    centery = player.bottom  
-            if event.key == pygame.K_UP:
-                centerx -=50
-                centery -=50
-                player.center = (centerx, centery)
+                positiony +=50
+                
+                coords = check_coordinates(positionx, positiony, width, height, RESCALE_WIDTH, RESCALE_HEIGHT)
+                print(coords)
+                positiony = coords[1]
+                position = (coords[0], coords[1])
+                
+                player = ball.get_rect(topleft=position)
+                
+                
+            
+            if event.key == pygame.K_LEFT:
+                positionx -=50
+                
+                coords = check_coordinates(positionx, positiony, width, height, RESCALE_WIDTH, RESCALE_HEIGHT)
+                print(coords)
+                positionx = coords[0]
+                position = (coords[0], coords[1])
+                player = ball.get_rect(topleft=position)
+                            
+            if event.key == pygame.K_RIGHT:
+                positionx +=50
+                
+                coords = check_coordinates(positionx, positiony, width, height, RESCALE_WIDTH, RESCALE_HEIGHT)
+                print(coords)
+                positionx = coords[0]
+                position = (coords[0], coords[1])
+                player = ball.get_rect(topleft=position)
+                
 
-                if player.left <0:
-                    player.left = 0
-                    centerx = player.left
-                if player.right >width:
-                    player.right = width
-                    centerx = player.right    
-                if player.top <0:
-                    player.top = 0
-                    centery = player.top
-                if player.bottom > height:
-                    player.bottom = height
-                    centery = player.bottom     
+            if event.key == pygame.K_UP:
+                positiony -=50
+                
+                coords = check_coordinates(positionx, positiony, width, height, RESCALE_WIDTH, RESCALE_HEIGHT)
+                print(coords)
+                positiony = coords[1]
+                position = (coords[0], coords[1])
+                player = ball.get_rect(topleft=position)
+                                 
     
     screen.fill(blue)
     #blit takes an object, and puts it on a surface
